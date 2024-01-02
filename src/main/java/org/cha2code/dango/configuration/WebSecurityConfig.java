85cb3bb73cc4ec5740379e0877e148e832971d3d
cha2jini <cha2jini@naver.com>
2024-01-02 11:10:36 +0900

    🔒 admin페이지 로그인 연결
    - admin 계정으로 로그인 시 admin 페이지로 리다이렉트 가능
    
    - user 계정은 admin 페이지 접속 불가

--- src/main/java/org/cha2code/dango/configuration/WebSecurityConfig.java
+++ src/main/java/org/cha2code/dango/configuration/WebSecurityConfig.java
@@ -6,10 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -17,11 +19,36 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
 		http.cors(AbstractHttpConfigurer::disable)
-		    .csrf(AbstractHttpConfigurer::disable);
+		    .csrf(AbstractHttpConfigurer::disable)
+		    .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
 
-		http.authorizeHttpRequests((req -> req.anyRequest().permitAll()))
-		    .formLogin(Customizer.withDefaults())
-		    .logout(Customizer.withDefaults());
+		http.authorizeHttpRequests((request -> request
+				    .requestMatchers(
+							new AntPathRequestMatcher("/common/**"),
+							new AntPathRequestMatcher("/dist/**"),
+							new AntPathRequestMatcher("/js/**"),
+							new AntPathRequestMatcher("/plugins"),
+							new AntPathRequestMatcher("/common/**")
+				    ).permitAll()
+				    .requestMatchers(
+						    new AntPathRequestMatcher("/")).hasAuthority("admin")
+				    .anyRequest().authenticated()
+		    ));
+
+
+		 http.formLogin(form -> form
+				 .loginPage("/login")
+				 .successHandler((request, response, authentication) -> {
+					 System.out.println("authentication : " + authentication.getName());
+					 response.sendRedirect("/");
+				 })
+				 .failureHandler((request, response, exception) -> {
+					 System.out.println("exception : " + exception.getMessage());
+					 response.sendRedirect("/login");
+				 })
+				 .permitAll());
+
+		        http.logout(Customizer.withDefaults());
 
 		return http.build();
 	}
@@ -29,10 +56,11 @@ public class WebSecurityConfig {
 	/*
 	* 하단 코드는 테스트용으로, DB연동 성공시 반드시 삭제할 것
 	*/
+
 	@Bean
 	public InMemoryUserDetailsManager userDetailsService() {
 		UserDetails admin = User.withUsername("admin")
-		                        .password("mQYqGpeEZwhPo0CvturXUoE/bno6Jq8o")
+		                        .password("$2a$10$tcuW33l.uYGHCNc5o6Gqc.LrQiGKHRx30/q.I6uTdXJNwx0juL4t.")
 		                        .authorities("admin")
 		                        .build();
 		UserDetails user = User.withUsername("user")
